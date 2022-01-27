4023f005bf8e7058c74a59301824357ee0d47f8c
Gilbert Ngeno <ngenogilbert07@gmail.com>
2022-01-27 12:27:09 +0300

    created packages &connected backend to mysql

---
+++ src/main/java/net/javaguides/springboot/SpringbootBackendFullApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendFullApplication.class, args);
+	}
+
+}
